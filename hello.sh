56c9e56890a779c32fb57a69dadaca3c8f17232b
Sarah Davis <sarahbethd@gmail.com>
2021-10-05 11:31:26 -0400

    random comment

---
+++ hello.sh
@@ -0,0 +1,24 @@
+
+
+
+
+# This is a comment
+#echo comment
+
+echo Hello
+echo World
+msg="Hello World"
+echo $msg
+echo What is your name?
+read userInput
+echo "Hello $userInput, Welcome to Programming!"
+for var in 1 2 3 4 5
+do
+	echo $var
+	echo This variable currently hold: $var
+done
+
+for words in Hello World Test Hi What
+do
+	echo $words
+msg = "Hola World"
