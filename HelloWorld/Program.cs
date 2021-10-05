da4bf1c986bf17418bdc30d166c97ba9054d6fc3
Sarah Davis <sarahbethd@gmail.com>
2021-10-05 17:44:49 -0400

    updated

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+// This is a comment//
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string name; //Variable for storing string value
+ 
+            //Displaying message for entring value
+            Console.WriteLine("Please Enter Your Good Name");
+ 
+            //Accepting and holding values in name variable
+            name = Console.ReadLine();
+
+        }
+    }
+}
